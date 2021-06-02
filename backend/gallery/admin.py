6dc10195c0b19451990df494b23bc24b0e10b91f
b-chae <cbr5964@naver.com>
2021-06-02 13:37:05 +0900

    MyGallery Model 만들기 1:N mapping

--- backend/gallery/admin.py
+++ backend/gallery/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
-from .models import GalleryPhoto
+from .models import GalleryPhoto, MyGallery
 
 
 @admin.register(GalleryPhoto)
 class GalleryPhotoAdmin(ModelAdmin):
+    pass
+
+@admin.register(MyGallery)
+class MyGalleryAdmin(ModelAdmin):
     pass
