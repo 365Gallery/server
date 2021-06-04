c4c46675f6c0d51660f59d62ad2264f3b049be96
b-chae <cbr5964@naver.com>
2021-06-04 23:06:14 +0900

    post & get tag field

--- backend/gallery/views.py
+++ backend/gallery/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from .serializers import PhotoSerializer
-from .models import GalleryPhoto
+from .models import GalleryPhoto, Tag
 from django.core.files.storage import default_storage
 import os
 from core.utils import Res
@@ -18,13 +18,17 @@ class GalleryViewSet(ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         file_obj = request.data.get('file')
+        tag_idx = request.data.get('tag_idx')
     
         print(file_obj)
 
         if file_obj == None or "image" not in file_obj.content_type:
             return Res.fail(400, "이미지가 아닙니다 ")
 
-        new_object = GalleryPhoto.objects.create(file = file_obj, src = "http://34.64.152.92:8000/" + file_obj.name.split('/')[-1])
+        new_object = GalleryPhoto.objects.create(file = file_obj)
+        new_object.tag = Tag.objects.get(idx=tag_idx)
+        new_object.save()
+        print(new_object.tag.idx)
 
         return Res.success("성공입니다", None)
 
