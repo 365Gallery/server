94c6fced11806706b1557bbece988ebc8df0f181
b-chae <cbr5964@sogang.ac.kr>
2021-06-04 23:12:05 +0900

    Features/gallery 갤러리 사진별 TAG 추가 (#5)
    * Tag model 추가
    
    * seed tags
    
    * post & get tag field

--- backend/gallery/serializers.py
+++ backend/gallery/serializers.py
@@ -1,10 +1,16 @@
-from .models import GalleryPhoto
+from .models import GalleryPhoto, Tag
 from rest_framework import serializers
+
+class TagField(serializers.RelatedField):
+    def to_representation(self, value):
+        return {'idx': value.idx, 'name': value.name, 'color': value.color}
 
 
 class PhotoSerializer(serializers.HyperlinkedModelSerializer):
     file = serializers.ImageField(use_url=True)
+    tag_idx = serializers.IntegerField(write_only=True)
+    tag = TagField(read_only=True)
 
     class Meta:
         model = GalleryPhoto
-        fields = ('pk', 'created', 'file', 'src')
+        fields = ('pk', 'created', 'file', 'tag_idx', 'tag')
